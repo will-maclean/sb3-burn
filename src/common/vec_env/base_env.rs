c29abfa3880f0db9eb44ef386cfb0c05f2c079f3
Will Maclean <macleanwill1@gmail.com>
2025-11-24 18:33:56 +1100

    finished dummy vec env and sub proc vec env, with tests

--- src/common/vec_env/base_env.rs
+++ src/common/vec_env/base_env.rs
@@ -1,16 +1,59 @@
 use crate::{
     common::spaces::Space,
-    env::base::{EnvObservation, ResetOptions, RewardRange},
+    env::base::{EnvObservation, Info, ResetOptions, RewardRange},
 };
 
-pub trait VecEnv<O, A> {
-    async fn step(&mut self, action: Vec<A>) -> Vec<EnvObservation<O>> {
-        self.step_async(action).await;
-        self.step_wait().await
+#[derive(Clone, Debug)]
+pub struct VecEnvObservation<O> {
+    pub obs: Vec<O>,
+    pub reward: Vec<f32>,
+    pub terminated: Vec<bool>,
+    pub truncated: Vec<bool>,
+    pub info: Vec<Info<O>>,
+    pub truncated_obs: Vec<Option<O>>,
+}
+
+impl<O> VecEnvObservation<O> {
+    pub fn new(mut obs_vec: Vec<EnvObservation<O>>, truncated_obs: Vec<Option<O>>) -> Self {
+        let mut obs = Vec::with_capacity(obs_vec.len());
+        let mut reward = Vec::with_capacity(obs_vec.len());
+        let mut terminated = Vec::with_capacity(obs_vec.len());
+        let mut truncated = Vec::with_capacity(obs_vec.len());
+        let mut info = Vec::with_capacity(obs_vec.len());
+
+        for _ in 0..obs_vec.len() {
+            let o = obs_vec.pop().unwrap();
+
+            obs.push(o.obs);
+            reward.push(o.reward);
+            terminated.push(o.terminated);
+            truncated.push(o.truncated);
+            info.push(o.info);
+        }
+
+        Self {
+            obs,
+            reward,
+            terminated,
+            truncated,
+            info,
+            truncated_obs,
+        }
     }
-    async fn step_async(&mut self, action: Vec<A>);
-    async fn step_wait(&mut self) -> Vec<EnvObservation<O>>;
-    async fn reset(&mut self, seed: Option<u64>, options: Option<ResetOptions>) -> Vec<O>;
+
+    pub fn len(&self) -> usize {
+        self.obs.len()
+    }
+}
+
+pub trait VecEnv<O, A> {
+    fn step(&mut self, action: Vec<A>) -> VecEnvObservation<O> {
+        self.step_async(action);
+        self.step_wait()
+    }
+    fn step_async(&mut self, action: Vec<A>);
+    fn step_wait(&mut self) -> VecEnvObservation<O>;
+    fn reset(&mut self, seed: Option<u64>, options: Option<ResetOptions>) -> Vec<O>;
     fn action_space(&self) -> Box<dyn Space<A>>;
     fn observation_space(&self) -> Box<dyn Space<O>>;
     fn reward_range(&self) -> RewardRange;
--- src/common/vec_env/dummy_vec_env.rs
+++ src/common/vec_env/dummy_vec_env.rs
@@ -1,17 +1,26 @@
 use std::mem;
 
 use crate::{
-    common::vec_env::base_env::VecEnv,
-    env::base::{Env, EnvObservation, ResetOptions},
+    common::vec_env::base_env::{VecEnv, VecEnvObservation},
+    env::base::{Env, ResetOptions},
 };
 
 pub struct DummyVecEnv<O, A> {
     envs: Vec<Box<dyn Env<O, A>>>,
-    cached_obs: Vec<EnvObservation<O>>,
+    cached_obs: Option<VecEnvObservation<O>>,
+}
+
+impl<O, A> DummyVecEnv<O, A> {
+    pub fn new(envs: Vec<Box<dyn Env<O, A>>>) -> Self {
+        Self {
+            envs,
+            cached_obs: None,
+        }
+    }
 }
 
 impl<O: Clone, A: Clone> VecEnv<O, A> for DummyVecEnv<O, A> {
-    async fn step_async(&mut self, action: Vec<A>) {
+    fn step_async(&mut self, action: Vec<A>) {
         if action.len() != self.envs.len() {
             panic!(
                 "Wrong amount of actions! Got {}, expecting {}",
@@ -21,17 +30,34 @@ impl<O: Clone, A: Clone> VecEnv<O, A> for DummyVecEnv<O, A> {
         }
 
         let mut new_obs = Vec::with_capacity(self.envs.len());
+        let mut truncated_obs = Vec::with_capacity(self.envs.len());
 
         for (i, a) in action.iter().enumerate() {
-            new_obs.push(self.envs[i].step(&a));
+            let mut obs = self.envs[i].step(&a);
+
+            if obs.truncated {
+                // store the termination
+                truncated_obs.push(Some(obs.obs.clone()));
+            } else {
+                truncated_obs.push(None);
+            }
+
+            if obs.truncated || obs.terminated {
+                obs.obs = self.envs[i].reset(None, None);
+            }
+
+            new_obs.push(obs);
         }
+
+        self.cached_obs = Some(VecEnvObservation::new(new_obs, truncated_obs));
     }
 
-    async fn step_wait(&mut self) -> Vec<EnvObservation<O>> {
-        mem::replace(&mut self.cached_obs, Vec::with_capacity(self.envs.len()))
+    fn step_wait(&mut self) -> VecEnvObservation<O> {
+        let obs = mem::replace(&mut self.cached_obs, None);
+        obs.unwrap()
     }
 
-    async fn reset(&mut self, seed: Option<u64>, options: Option<ResetOptions>) -> Vec<O> {
+    fn reset(&mut self, seed: Option<u64>, options: Option<ResetOptions>) -> Vec<O> {
         self.envs
             .iter_mut()
             .map(|e| e.reset(seed, options.clone()))
@@ -60,5 +86,37 @@ impl<O: Clone, A: Clone> VecEnv<O, A> for DummyVecEnv<O, A> {
 
     fn close(&mut self) {
         self.envs.iter_mut().for_each(|e| e.close());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        common::vec_env::{base_env::VecEnv, dummy_vec_env::DummyVecEnv},
+        env::{base::Env, classic_control::cartpole::CartpoleEnv},
+    };
+
+    #[test]
+    fn test_dummy_basic() {
+        let envs: Vec<Box<dyn Env<Vec<f32>, usize>>> = vec![
+            Box::new(CartpoleEnv::default()),
+            Box::new(CartpoleEnv::default()),
+            Box::new(CartpoleEnv::default()),
+        ];
+
+        let mut vec_env = DummyVecEnv::new(envs);
+        let mut action_space = vec_env.action_space();
+
+        let reset_obs = vec_env.reset(None, None);
+
+        assert_eq!(reset_obs.len(), 3);
+
+        for _ in 0..10 {
+            let act = (0..3).map(|_| action_space.sample()).collect();
+            let obs = vec_env.step(act);
+            assert_eq!(obs.len(), 3);
+        }
+
+        vec_env.close();
     }
 }
